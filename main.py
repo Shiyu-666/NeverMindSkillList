6d14e458c93c04b27d856ee1c83efe652b910610
Zyyans <1980797568@qq.com>
2020-03-04 02:22:34 +0800

    整理了一下思路, 大概好起来了?

---
+++ main.py
@@ -0,0 +1,26 @@
+from mechanics import *
+
+
+def is_float(num):
+    return False if num.count('.') > 1 else num.replace('.', '').isdigit()
+
+
+def attribute_edit(attrs):
+
+    limit = attrs[4]
+
+    print('  正在编辑 %s 参数.\n  参数描述: %s.' % (attrs[0], attrs[2]))
+    print('  参数限制: ' + {
+        'bool': 'True / False', 'float': '数字', 'int': '整数', 'str': '字符串'
+    }[limit])
+    res = input('  请输入您想要的参数值 >> ')
+
+    while (
+        limit == 'bool' and res not in ['True', 'False']) or (
+        limit == 'int' and not res.isdigit()) or (
+        limit == 'float' and not is_float(res)) or (
+        limit == 'str' and not res
+    ):
+        res = input('  输入有误, 请重新输入 >> ')
+
+    return res
